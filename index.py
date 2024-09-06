72b4495fa6e36db674807569b9e8c5a9505c3fbd
João Pedro Binelo <112009925+Binelo@users.noreply.github.com>
2024-09-05 21:54:32 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -10,6 +10,9 @@ class Vm:
         self.P = P
         self.done = done
 
+    def __repr__(self) -> str:
+        return f'{self.ID}'
+
     def info(self):
         print("ID: ", self.ID)
         print("vCPU: ", self.vCPU)
@@ -17,8 +20,8 @@ class Vm:
         print("Arrival Time: ", self.AT)
         print("Priority: ", self.P)
 
-vm1 = Vm("Vm1", 4, 10, 0, 3, False)
-vm2 = Vm("Vm2", 1, 10, 35, 1, False)
+vm1 = Vm("Vm1", 2, 10, 0, 3, False)
+vm2 = Vm("Vm2", 1, 10, 20, 1, False)
 vm3 = Vm("Vm3", 1, 10, 30, 2, False)
 vm4 = Vm("Vm4", 1, 10, 40, 2, False)
 vm5 = Vm("Vm5", 1, 25, 35, 3, False)
@@ -26,25 +29,20 @@ vm6 = Vm("Vm6", 2, 15, 10, 2, False)
 vm7 = Vm("Vm7", 2, 25, 10, 1, False)
 vm8 = Vm("Vm8", 4, 5, 40, 1, False)
 vm9 = Vm("Vm9", 1, 10, 35, 0, False)
-vm10 = Vm("Vm10", 4, 30, 0, 2, False)
+vm10 = Vm("Vm10", 2, 30, 0, 2, False)
 
 lista_vms = [vm1, vm2, vm3, vm4, vm5, vm6, vm7, vm8, vm9, vm10]
 all_done = False
-escala = [0, 0, 0, 0]
-espaco = escala.count(0)
+lista_espera = []
+escala = []
+espaco = 4 - len(escala)
 TIME = 0
 
 def preenche_lista(vm):
-    
-    if espaco - vm.vCPU <= 4:
+    if espaco - vm.vCPU >= 0:
         for i in range(0, vm.vCPU):
             escala.append(vm.ID)
-            escala.remove(0)
-        n = 4 - len(escala)
-        while n > 0:
-            escala.append(0)
-            n = n - 1
-        return(escala)
+        return escala
     else:
         return("n deu")
 
@@ -65,40 +63,43 @@ def exec():
     
 
 def decidir_vm_para_executar(lista, total_cpus):
+    vm_escolhida = lista[0]
+    for i in range(0, len(lista) - 1):
     
-    for i in range(0, len(lista)):
-    
-        if lista[i].vCPU == total_cpus and lista[i + 1].vCPU == total_cpus:
+        if vm_escolhida.vCPU == total_cpus and lista[i + 1].vCPU == total_cpus:
             # Executa a de maior prioridade
-            if lista[i].P < lista[i + 1].P:
-                return lista[i].ID
+            if vm_escolhida.P < lista[i + 1].P:
+                vm_escolhida = vm_escolhida
             else:
-                return lista[i + 1].ID
-        elif lista[i].vCPU == total_cpus:
-            return lista[i].ID
+                vm_escolhida = lista[i + 1]
+        elif vm_escolhida.vCPU == total_cpus:
+            vm_escolhida = vm_escolhida
         elif lista[i + 1].vCPU == total_cpus:
-            return lista[i + 1].ID
+            vm_escolhida = lista[i + 1]
 
-    # Se a diferença de prioridade for 1 e a VM com menor prioridade é executável em menos da metade do tempo
-    if abs(vm1.P - vm2.P) == 1:
-        if vm1.P > vm2.P and vm1.ET < vm2.ET / 2:
-            return vm1.ID
-        elif vm2.P > vm1.P and vm2.ET < vm1.ET / 2:
-            return vm2.ID
+        # Se a diferença de prioridade for 1 e a VM com menor prioridade é executável em menos da metade do tempo
+        if abs(vm_escolhida.P - lista[i + 1].P) == 1:
+            if vm_escolhida.P > lista[i + 1].P and vm_escolhida.ET < lista[i + 1].ET / 2:
+                vm_escolhida = vm_escolhida
+            elif lista[i + 1].P > vm_escolhida.P and lista[i + 1].ET < vm_escolhida.ET / 2:
+                vm_escolhida = lista[i + 1]
 
-    # Se a soma das vCPUs for maior que as CPUs disponíveis
-    if vm1.vCPU + vm2.vCPU > total_cpus:
-        # Executa a VM de menor prioridade, desde que ela ocupe menos CPUs
-        if vm1.P < vm2.P and vm1.vCPU < vm2.vCPU:
-            return vm1.ID
-        elif vm2.P < vm1.P and vm2.vCPU < vm1.vCPU:
-            return vm2.ID
+        # Se a soma das vCPUs for maior que as CPUs disponíveis
+        if vm_escolhida.vCPU + lista[i + 1].vCPU > total_cpus:
+            # Executa a VM de menor prioridade, desde que ela ocupe menos CPUs
+            if vm_escolhida.P < lista[i + 1].P and vm_escolhida.vCPU < lista[i + 1].vCPU:
+                vm_escolhida = vm_escolhida
+            elif lista[i + 1].P < vm_escolhida.P and lista[i + 1].vCPU < vm_escolhida.vCPU:
+                vm_escolhida = lista[i + 1]
 
-    # Caso contrário, executa a VM de maior prioridade
-    if vm1.P < vm2.P:
-        return vm1.ID
-    else:
-        return vm2.ID
+        # Caso contrário, executa a VM de maior prioridade
+        if vm_escolhida.P < lista[i + 1].P:
+            vm_escolhida = vm_escolhida
+        else:
+            vm_escolhida = lista[i + 1]
+    lista.remove(vm_escolhida)
+    lista_espera.extend(lista)
+    return vm_escolhida
 
 def conta_done():
     cd = 0
@@ -110,7 +111,9 @@ def conta_done():
 
 while not all_done:
     lista_at = exec()
-    print(preenche_lista(vm2))
-    # if len(lista_at) == 1:
-    #     preenche_lista(lista_at[0])
+    if len(lista_at) == 1:
+        preenche_lista(lista_at[0])
+    elif len(lista_at) > 1:
+        preenche_lista(decidir_vm_para_executar(lista_at, espaco))
+    print(escala)
     all_done = True
